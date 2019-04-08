d277d918c4d6421731621c1a4f1fee6d3a86afae
Brian Do <thebriando@gmail.com>
2019-04-08 09:52:30 -0700

    initial push

---
+++ app/src/main/java/com/example/briando/hello/MainActivity.kt
@@ -0,0 +1,12 @@
+npackage com.example.briando.hello
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
